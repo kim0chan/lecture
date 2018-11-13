d5b84b1fa2c8cae8145b0a19a456e8081eaa2666
kim0chan <[1999@korea.ac.kr]>
2018-11-13 11:14:08 +0900

    제발요

--- src/hollydollailer/ahngyo.java
+++ src/hollydollailer/ahngyo.java
@@ -3,6 +3,6 @@ package hollydollailer;
 public class ahngyo {
 	public static void main(String[] args) {
 		System.out.println("안녕아세요");
-		System.out.println("안녕이가새요");
+		System.out.println("안녕이가d새요");
 	}
 }
